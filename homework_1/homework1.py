88c20a6d6aaffcf0adecd562bacbce5852023027
Gideon I. Glass <gglass@scrippsnetworks.com>
2015-09-02 14:41:18 -0400

    Function to parse training data

--- homework_1/homework1.py
+++ homework_1/homework1.py
@@ -1,2 +1,31 @@
 # Digit Recognizer
-#
+
+import numpy as np
+import scipy as sp
+import sys
+import csv
+
+# Parse dataset
+def parse_data (file_path):
+    try:
+        input = open(file_path)
+        labels, digits = [], []
+        try:
+            reader = csv.reader(input)
+            for row in reader:
+                labels.append(row[0])
+                pixels = [row[i:i+28] for i in range(1, len(row), 28)]
+                digits.append(np.matrix(pixels))
+        finally:
+            input.close()
+            return (labels, digits)
+    except IOError:
+        print "Error - please check filename and try again.\n"
+        sys.exit(1)
+
+
+# TODO Write a function to display MNIST digit
+def display_digit (pixel_matrix):
+    # USE SCIPY TOIMAGE
+
+data_set = parse_data('./train.csv')
