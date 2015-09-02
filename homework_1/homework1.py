8c05bfa92a7af4c16740440c59c1465745b26b4f
Gideon I. Glass <gglass@scrippsnetworks.com>
2015-09-02 17:56:53 -0400

    Added function to save a digit

--- homework_1/homework1.py
+++ homework_1/homework1.py
@@ -1,9 +1,10 @@
 # Digit Recognizer
 
 import numpy as np
-import scipy as sp
 import sys
 import csv
+import scipy.misc
+import matplotlib.image
 
 # Parse dataset
 def parse_data (file_path):
@@ -15,17 +16,24 @@ def parse_data (file_path):
             for row in reader:
                 labels.append(row[0])
                 pixels = [row[i:i+28] for i in range(1, len(row), 28)]
-                digits.append(np.matrix(pixels))
+                digits.append(np.array(pixels))
         finally:
             input.close()
-            return (labels, digits)
+            return {'labels': labels, 'digits': digits}
     except IOError:
         print "Error - please check filename and try again.\n"
         sys.exit(1)
 
-
-# TODO Write a function to display MNIST digit
-def display_digit (pixel_matrix):
-    # USE SCIPY TOIMAGE
-
+# Write a function to display MNIST digit
+def save_image (pixel_matrix, index):
+    index += 1
+    matplotlib.image.imsave('digit_' + str(index), data)
+    return
+    
 data_set = parse_data('./train.csv')
+
+# display_digit(data, 1)
+
+
+
+
